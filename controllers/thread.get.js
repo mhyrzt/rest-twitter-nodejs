315ae4b3e1306336067707241800bbcc23c289fe
mahyar <mr.riazati1999@gmail.com>
2022-10-12 18:03:49 +0330

    bug fix

--- controllers/thread.get.js
+++ controllers/thread.get.js
@@ -2,9 +2,13 @@ const Tweet = require("../models/Tweet/methods.js");
 
 function register({ app }, path = "/api/tweets/:tweet_id") {
 	app.get(path, async function (req, res) {
-		const { tweet_id } = req.params;
-		const thread = await Tweet.getThread(tweet_id);
-		res.send({ tweet_id, thread });
+		try {
+			const { tweet_id } = req.params;
+			const thread = await Tweet.getThread(tweet_id);
+			res.send({ tweet_id, thread });
+		} catch (err) {
+			res.status(404);
+		}
 	});
 }
 
